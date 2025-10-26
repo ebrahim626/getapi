b3efd1b893f831a7808e5de45b782bb1511aa394
Ebrahim Hossain <ebrahimhossain626@gmail.com>
2025-10-26 19:07:07 +0600

    updated

--- lib/api/post_provider.dart
+++ lib/api/post_provider.dart
@@ -1,10 +1,16 @@
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:getapi/model/post_model.dart';
+import 'package:http/http.dart' as http;
 
 final postProvider = FutureProvider<List<PostModel>>((ref) async {
   try{
-
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    if(response.statusCode == 200){
+      return ;
+    }else {
+      throw 'Something went wrong.';
+    }
   }catch(e){
     rethrow;
   }

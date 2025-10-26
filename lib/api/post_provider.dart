bd3df1c761ddcfd142f884eaaac0afaf90f5e84f
Ebrahim Hossain <ebrahimhossain626@gmail.com>
2025-10-26 19:00:17 +0600

    updated

---
+++ lib/api/post_provider.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:getapi/model/post_model.dart';
+
+final postProvider = FutureProvider<List<PostModel>>((ref) async {
+  try{
+
+  }catch(e){
+    rethrow;
+  }
+});
